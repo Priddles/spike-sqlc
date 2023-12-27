796b31b964f96b0f997611de2d57826ba661319e
Kai Pridham <kai.pridham@kablamo.com.au>
2023-12-27 20:02:32 +1100

    format

--- queries/foo.sql
+++ queries/foo.sql
@@ -1,20 +1,14 @@
 -- name: GetFoo :one
-SELECT * FROM foo
-WHERE id = $1
-LIMIT 1;
+SELECT * FROM foo WHERE id = $1 LIMIT 1;
 
 -- name: ListFoos :many
-SELECT
-    id,
+SELECT id,
     area,
     (CASE WHEN @return_geometry::boolean THEN location ELSE null END)::geom_point as location
 FROM foo
 WHERE location && @bound::geom_bound;
 
 -- name: CreateFoo :one
-INSERT INTO foo (
-    id, location
-) VALUES (
-    $1, $2
-)
+INSERT INTO foo (id, location)
+VALUES ($1, $2)
 RETURNING *;
